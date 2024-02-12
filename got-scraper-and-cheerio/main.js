7c69ae59aa52c548e4d9f44b6199c44c7a20d8b1
Holy Mark <mkmark53@gmail.com>
2024-02-12 19:35:34 +0000

    s

--- got-scraper-and-cheerio/main.js
+++ got-scraper-and-cheerio/main.js
@@ -1,6 +1,7 @@
 import { gotScraping } from "got-scraping";
-
+import { writeFileSync } from "fs";
 import * as cheerio from "cheerio";
+import { parse } from "json2csv";
 
 const storeUrl =
   "https://warehouse-theme-metal.myshopify.com/collections/sales";
@@ -12,13 +13,15 @@ const storeUrl =
 
   const $ = cheerio.load(html);
   const products = $(".product-item"); // all of em
+  let results = [];
   for (const product of products) {
-    let results = [];
     const title = $(product).find("a.product-item__title").text().trim();
     const price = $(product).find("span.price").contents()[2].nodeValue.trim();
 
     results.push({ title, price });
-
-    console.log(results);
   }
+  const csv = parse(results);
+
+  writeFileSync(".csv", csv);
+  // console.log(csv);
 })(storeUrl);
