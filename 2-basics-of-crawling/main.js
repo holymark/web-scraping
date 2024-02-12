8b2da9da27d3fcb47c72f935727d05cbb9b8373f
Holy Mark <mkmark53@gmail.com>
2024-02-12 20:56:43 +0000

    ....

--- 2-basics-of-crawling/main.js
+++ 2-basics-of-crawling/main.js
@@ -7,14 +7,46 @@ const storeUrl = `${BASE__URL}/collections/sales`;
   const response = await gotScraping(url);
   const html = response.body;
   const $ = cheerio.load(html);
-  const regExp =
-    /https?:\/\/warehouse-theme-metal\.myshopify\.com\/products\/[\w-]+/;
 
-  const links = $("a");
+  const productLinks = $("a.product-item__title");
 
-  for (const link of links) {
+  const productsURLs = [];
+
+  for (const link of productLinks) {
     const url = $(link).attr("href");
- const absoluteUrl=new URL(url,BASE__URL)
-    if (regExp.test(url)) console.log(url);
+    const absoluteUrl = new URL(url, BASE__URL);
+    productsURLs.push(absoluteUrl);
+  }
+  for (const productURL of productsURLs) {
+    try {
+      const productResponse = await gotScraping(productURL);
+      const productHtml = productResponse.body;
+      const $productPage = cheerio.load(productHtml);
+
+      const productPageTitle = $productPage("h1").text().trim();
+      const productVendor = $productPage("a.product-meta__vendor")
+        .text()
+        .trim();
+      const productPrice = $productPage("span.price").contents()[2].nodeValue;
+      const productReviewCount = parseInt(
+        $productPage("span.rating__caption").text(),
+        10
+      );
+
+      const productDescription = $('div[class*="description"] div.rte')
+        .text()
+        .trim();
+
+      const productItem = {
+        productPageTitle,
+        productVendor,
+        productPrice,
+        productReviewCount,
+        productDescription,
+      };
+      console.log(productItem);
+    } catch (e) {
+      console.log(e.message, productURL);
+    }
   }
 })(storeUrl);
