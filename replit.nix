b3a49fba8e1b528dc11b505ed016cbba0c1941d7
Replit user <>
2024-02-15 20:33:58 +0000

    newest

--- replit.nix
+++ replit.nix
@@ -1,3 +1,10 @@
-{pkgs}: {
-  deps = [ ];
+{ pkgs }: {
+	deps = [
+				pkgs.nodejs-16_x
+				pkgs.nodePackages.typescript-language-server
+				pkgs.yarn
+				pkgs.replitPackages.jest
+				pkgs.chromium
+				pkgs.firefox
+	];
 }
